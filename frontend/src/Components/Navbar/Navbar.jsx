6e50415b3fbe09fef3b23d79d468125b14085585
Sush-psusmitha <sushsusmitha13@gmail.com>
2025-12-14 03:20:20 +0530

    'test'

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -2,31 +2,66 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
+
 const Navbar = () => {
-    const[menu,setMenu] = useState("Shop")
+  const [menu, setMenu] = useState("shop")
+  const [open, setOpen] = useState(false)
+
   return (
-    <div className='navbar'>
-      <div className='nav-logo'>
-        <img src={logo} alt="" />
+    <>
+    <div className="navbar">
+
+      {/* Logo */}
+      <div className="nav-logo">
+        <img src={logo} alt="logo" />
         <p>SHOPPER</p>
       </div>
-    <ul class="nav-menu">
-        <li onClick={()=>{setMenu("shop")}}>Shop {menu === "shop"? <hr/> :<></>}</li>
-        <li onClick={()=>{setMenu("mens")}}>Men {menu === "mens"? <hr/> :<></>}</li>
-        <li onClick={()=>{setMenu("womens")}}>Women {menu === "womens"? <hr/> :<></>}</li>
-        <li onClick={()=>{setMenu("kids")}}>Kids {menu === "kids"? <hr/> :<></>}</li>
-    </ul>
 
-    <div class="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt=""/>
+      {/* Menu */}
+      <ul className={`nav-menu ${open ? "active" : ""}`}>
+        <li onClick={() => setMenu("shop")}>
+          <Link to="/">Shop</Link>
+          {menu === "shop" && <hr />}
+        </li>
 
-        <div class="nav-cart-count">
-            0
+        <li onClick={() => setMenu("Men")}>
+          <Link to="/mens">Men</Link>
+          {menu === "Men" && <hr />}
+        </li>
+
+        <li onClick={() => setMenu("Women")}>
+          <Link to="/womens">Women</Link>
+          {menu === "Women" && <hr />}
+        </li>
+
+        <li onClick={() => setMenu("Kids")}>
+          <Link to="/kids">Kids</Link>
+          {menu === "Kids" && <hr />}
+        </li>
+      </ul>
+
+      {/* Login, Cart & Hamburger */}
+      <div className="nav-login-cart">
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+
+        <Link to="/cart" className="cart-wrapper">
+          <img src={cart_icon} alt="cart" />
+          <div className="nav-cart-count">0</div>
+        </Link>
+
+        {/* Hamburger at END */}
+        <div className="hamburger" onClick={() => setOpen(!open)}>
+          ☰
         </div>
+      </div>
+
     </div>
-    </div>
+    </>
+    
   )
-} 
+}
 
 export default Navbar
