e87bc86f764e5a9986267799c2cbf1eec6216a1f
Sush-psusmitha <sushsusmitha13@gmail.com>
2025-12-18 23:17:18 +0530

    test

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './ProductDisplay.css'
 import star_icon from '../Assets/star_icon.png'
+import star_dull from '../Assets/star_dull_icon.png'
 const ProductDisplay = (props) => {
     const {product} = props;
   return (
@@ -19,8 +20,41 @@ const ProductDisplay = (props) => {
       <div className="productdisplay-Right">
            <h1>{product.name}</h1>
            <div class="productdisplay-right-star">
-            <img src={} alt=""/>
+            <img src={star_icon} alt=""/>
+            <img src={star_icon} alt=""/>
+            <img src={star_icon} alt=""/>
+            <img src={star_icon} alt=""/>           
+            <img src={star_dull} alt=""/>
+            <p>(122)</p>
            </div>
+           <div class="productdisplay-right-prices">
+            <div class="productdisplay-right-price-old">${product.old_price}
+            </div>
+            <div class="productdisplay-right-price-new">${product.new_price}
+            </div>
+           </div>
+           <div class="productdisplay-right-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint magni harum fugit nobis impedit laboriosam est cupiditate quia atque mollitia.
+           </div>
+           <div className='productdisplay-right-size'> 
+             <h1>Select Size</h1>
+             <div class="productdisplay-right-size">
+                <div>S</div>
+                <div>M</div>
+                <div>L</div>
+                <div>XL</div>
+                <div>XXL</div>
+             </div>
+           </div>
+           <button>ADD TO CART</button>
+           <p className='productdisplay-right-category
+           '>
+            <span>Category: </span> Women, T-Shirt, Crop Top
+           </p>
+           <p className='productdisplay-right-category
+           '>
+            <span>Tags: </span> Modern, Latest, 
+           </p>
       </div>
     </div>
   )
